cac7cafa5266b5bd55ebe496e444b48124021f9a
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-05-11 15:43:35 +0200

    visual improvements

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
@@ -13,21 +14,19 @@
     <h2>You are logged in</h2>
     <form action="/logout" method="post">
         @csrf
-        <button type="submit" style="font-size: 2em; font-weight: bold;">Log out</button>
+        <button type="submit">Log out</button>
     </form>
-    <div id="post" style="border: 8px solid black; margin: 2em;">
-        <div style="background-color: lightgreen; margin: 1em; padding: 1em;">
-            <h1>Editing post</h1>
-            <form action="/edit-post/{{$post->id}}" method="post">
-                @csrf
-                @method('put')
-                <label for="title">Title:</label>
-                <input name="title" type="text" value="{{$post->title}}">
-                <label for="content">Content:</label>
-                <textarea name="content" type="text">{{$post->content}}</textarea>
-                <button type="submit" style="font-size: 2em; font-weight: bold;">Edit</button>
-            </form>
-        </div>
+    <div id="post">
+        <h1>Editing post</h1>
+        <form action="/edit-post/{{$post->id}}" method="post">
+            @csrf
+            @method('put')
+            <label for="title">Title:</label>
+            <input name="title" type="text" value="{{$post->title}}">
+            <label for="content">Content:</label>
+            <textarea name="content" type="text">{{$post->content}}</textarea>
+            <x-buttons.edit />
+        </form>
     </div>
     @else
     <h2>You are NOT logged in</h2>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
@@ -13,33 +14,33 @@
     <h2>You are logged in</h2>
     <form action="/logout" method="post">
         @csrf
-        <button type="submit" style="font-size: 2em; font-weight: bold;">Log out</button>
+        <button type="submit">Log out</button>
     </form>
-    <div id="posts" style="border: 8px solid black; margin: 2em;">
+    <div id="create-posts">
         <form action="/create-post" method="post">
             @csrf
             <label for="title">Title:</label>
             <input name="title" type="text" placeholder="title">
             <label for="content">Content:</label>
             <textarea name="content" type="text" placeholder="content"></textarea>
-            <button type="submit" style="font-size: 2em; font-weight: bold;">Post</button>
+            <x-buttons.add />
         </form>
     </div>
     <h1>Your posts</h1>
-    <div id="posts" style="border: 8px solid black; margin: 2em;">
+    <div id="posts">
         @foreach($posts as $post)
-        <div style="background-color: lightgreen; margin: 1em; padding: 1em;">
+        <div class="post">
             <h3>{{$post->user->name}}: {{$post['title']}}</h3>
             <span>{{$post['content']}}</span>
-            <div style="display: grid; grid-template-columns: 1fr 1fr; width: 20%;">
+            <div class="float-right flex">
                 <form action="/edit-post/{{$post->id}}" method="get">
                     @csrf
-                    <button type="submit">Edit</button>
+                    <x-buttons.edit />
                 </form>
                 <form action="/delete-post/{{$post->id}}" method="post">
                     @csrf
                     @method('delete')
-                    <button type="submit">Delete</button>
+                    <x-buttons.delete />
                 </form>
             </div>
         </div>
@@ -47,8 +48,7 @@
     </div>
     @else
     <h2>Register</h2>
-    <form action="/register" method="post"
-        style="display: grid; grid-template-columns: 1fr 1fr; width: 40%;">
+    <form action="/register" method="post">
         @csrf
         <label for="name">Username:</label>
         <input name="name" type="text" placeholder="name">
@@ -56,18 +56,17 @@
         <input name="email" type="text" placeholder="email">
         <label for="password">Password:</label>
         <input name="password" type="password" placeholder="password">
-        <button type="submit" style="font-size: 2em; font-weight: bold;">Submit</button>
+        <button type="submit">Submit</button>
     </form>
 
     <h2>Log In</h2>
-    <form action="/login" method="post"
-        style="display: grid; grid-template-columns: 1fr 1fr; width: 40%;">
+    <form action="/login" method="post">
         @csrf
         <label for="loginname">Username:</label>
         <input name="loginname" type="text" placeholder="name">
         <label for="loginpassword">Password:</label>
         <input name="loginpassword" type="password" placeholder="password">
-        <button type="submit" style="font-size: 2em; font-weight: bold;">Log in</button>
+        <button type="submit">Log in</button>
     </form>
     @endauth
 </body>
